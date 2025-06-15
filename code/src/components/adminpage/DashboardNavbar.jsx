b9bef51dea4185e46cdedd997ad9239878c40c44
Ondřej Malík <Onndrejmalik@gmail.com>
2025-06-15 21:22:17 +0200

    Project add button implemented

--- code/src/components/adminpage/DashboardNavbar.jsx
+++ code/src/components/adminpage/DashboardNavbar.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { FaHome, FaEnvelope, FaSignOutAlt, FaUser, FaClipboardList } from "react-icons/fa";
+import {
+  FaHome,
+  FaEnvelope,
+  FaSignOutAlt,
+  FaUser,
+  FaClipboardList,
+} from "react-icons/fa";
 import { useAuth } from "../../context/AuthContext2";
 import ModalLogout from "../Modals/ModalLogout";
 
 const DashboardNavbar = () => {
-  const SideBarIcon = ({ icon }) => <div className="dashboard-sidebar-icon">{icon}</div>;
+  const SideBarIcon = ({ icon }) => (
+    <div className="dashboard-sidebar-icon">{icon}</div>
+  );
   const [open, setOpen] = useState(false);
   const { logout } = useAuth();
 
@@ -15,11 +23,7 @@ const DashboardNavbar = () => {
       <nav className="bg-primary fixed top-0 w-full border-b border-darkSecondary shadow-md z-1">
         <ul className="w-full top-0 h-[75px] list-none flex items-center my-2">
           <li className="mr-auto mx-4 z-50">
-            <img
-              src="/imgs/warp-logo.png"
-              alt=""
-              className="logo"
-            />
+            <img src="/imgs/warp-logo.png" alt="" className="logo" />
           </li>
         </ul>
       </nav>
@@ -38,19 +42,15 @@ const DashboardNavbar = () => {
         </div>
 
         {/* Bottom (Logout) icon */}
-        <div
-          className="mb-4"
-          onClick={() => setOpen(true)}
-        >
+        <div className="mb-4" onClick={() => setOpen(true)}>
           <SideBarIcon icon={<FaSignOutAlt size="28" />} />
 
           {/*MODAL LOGOUT*/}
-          <ModalLogout
-            open={open}
-            onClose={() => setOpen(false)}
-          >
+          <ModalLogout open={open} onClose={() => setOpen(false)}>
             <h4 className="heading2 text-center mt-16">Odhlásit se?</h4>
-            <p className="paragraph text-gray-200 text-center mt-4">Opravdu si přejete se odhlásit?</p>
+            <p className="paragraph text-gray-200 text-center mt-4">
+              Opravdu si přejete se odhlásit?
+            </p>
             <div className="flex flex-row justify-center mt-20 gap-4">
               <button
                 onClick={logout}
